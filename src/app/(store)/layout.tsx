b34758f649f497287472334dd0f6bb37b1bba4d9
ymillerr <millerandvitor@gmail.com>
2023-11-09 11:41:17 +0100

    feat: create Header component

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,9 +1,10 @@
+import { Header } from '@/components/Header'
 import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
     <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
-      <h1>Header component...</h1>
+      <Header />
       {children}
     </div>
   )
