45829ae9c6be4c617d89be0b53bc8c86a22aa56b
ymillerr <millerandvitor@gmail.com>
2023-11-14 09:35:19 +0100

    feat: create product page

---
+++ src/app/(store)/product/[slug]/page.tsx
@@ -0,0 +1,129 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
+
+import Image from 'next/image'
+
+interface ProductProps {
+  params: {
+    slug: string
+  }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const products = await response.json()
+
+  return products
+}
+
+export async function generateMetadata({
+  params: { slug },
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(slug)
+
+  return {
+    title: product.title,
+  }
+}
+
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map((product) => {
+    return {
+      slug: product.slug,
+    }
+  })
+}
+
+export default async function ProductPage({ params: { slug } }: ProductProps) {
+  const product = await getProduct(slug)
+
+  return (
+    <div className="relative grid max-860px grid-cols-3">
+      <div className="col-span-2 overflow-hidden">
+        <Image
+          src={product.image}
+          width={1000}
+          height={1000}
+          quality={100}
+          alt=""
+        />
+      </div>
+
+      <div className="flex flex-col justify-center px-12">
+        <h1 className="tex-3xl font-bold leading-tight">{product.title}</h1>
+
+        <p className="mt-2 leading-relaxed text-zinc-400">
+          {product.description}
+        </p>
+
+        <div className="mt-8 flex items-center gap-3">
+          <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
+            {product.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              maximumFractionDigits: 0,
+              minimumFractionDigits: 0,
+            })}
+          </span>
+          <span className="text-sm text-zinc-400">
+            Em 12 s/ juros de{' '}
+            {(product.price / 12).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </span>
+        </div>
+
+        <div className="mt-8 space-y-4">
+          <span className="block font-semibold">Tamanhos</span>
+
+          <div className="flex gap-2">
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              P
+            </button>
+
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              M
+            </button>
+
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              G
+            </button>
+
+            <button
+              type="button"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              GG
+            </button>
+          </div>
+        </div>
+
+        <button
+          type="button"
+          className="text-white mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold"
+        >
+          Adicionar ao carrinho
+        </button>
+      </div>
+    </div>
+  )
+}
